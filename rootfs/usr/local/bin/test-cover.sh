66b008c2ab1e0883b0b4dfe31979e559eef951db
duanhongyi <duanhongyi@doopai.com>
2024-06-04 16:00:01 +0800

    chore(test-cover): add codecov

--- rootfs/usr/local/bin/test-cover.sh
+++ rootfs/usr/local/bin/test-cover.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 go test --covermode=atomic -coverprofile=coverage.txt ./...
+if [[ -n $CODECOV_TOKEN ]]; then
+  codecov upload-process --plugin=noop -t "$CODECOV_TOKEN"
+fi
